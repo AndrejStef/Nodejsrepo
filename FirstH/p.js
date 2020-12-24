d0f1dade561ec637853df761529579bfd78a8eb0
AndrejStef <astefanoski93@gmail.com>
2020-12-24 15:33:31 +0100

    OK

---
+++ FirstH/p.js
@@ -0,0 +1,19 @@
+function personInformation (persons) {
+    const information = `Person information: 
+    ${persons.firstName} 
+    ${persons.lastName} 
+    ${persons.age} 
+    ${persons.gender} 
+    ${persons.ssn} 
+    ${persons.country} 
+    ${persons.city}`;
+
+    return information;
+}
+
+function getLocation(){
+    console.log(`${persons.country} ${person.city}`);
+}
+module.exports = {
+    personInformation
+}
